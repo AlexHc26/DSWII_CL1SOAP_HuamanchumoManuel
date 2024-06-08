cca2ec650c89c9c2f81a5c8d73bcc495c1b5c8ac
AlexHc26 <mahc2601@gmail.com>
2024-06-08 09:09:26 -0500

    primer commit

---
+++ src/main/java/pe/edu/cibertec/DSWII_CL1SOAP_HuamanchumoManuel/DswiiCl1SoapHuamanchumoManuelApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_CL1SOAP_HuamanchumoManuel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiCl1SoapHuamanchumoManuelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiCl1SoapHuamanchumoManuelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DSWII_CL1SOAP_HuamanchumoManuel/DswiiCl1SoapHuamanchumoManuelApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_CL1SOAP_HuamanchumoManuel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiCl1SoapHuamanchumoManuelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
